598cdae37cada7d2874ab59136f1b6f42ec4e152
Joerg Lenhard <joerg.lenhard@kau.se>
2017-07-24 12:19:32 +0200

    Improved code documentation for the release

--- classifiers.R
+++ classifiers.R
@@ -2,12 +2,16 @@
 ####### Builds Naive Bayes classifiers for the data in data-frame.csv ###########
 #################################################################################
 
-# First load the data file. You need to adjust the file path to your local environment
+# Instructions:
+# To perform the computation, load the script in your R environment and adjust it to what you want to compute.
+# Hints on possible adjustments are given as comments in this file.
+
+# First load the data file. You need to adjust the file path to your local environment.
 
 dataFrameFile <- "C:/workspaces/git/saerocon2017-replication/data-frame.csv"
 data <- read.csv(dataFrameFile, sep=";")
 
-# Load the RWeka library (needs to be installed in your environment) and the build naive bayes
+# Load the RWeka library (which needs to be installed in your environment) and the build naive bayes classifier
 library(RWeka)
 NB <- make_Weka_classifier("weka/classifiers/bayes/NaiveBayes")
 # Set up the vector of metrics used in the analysis
@@ -15,38 +19,47 @@ miningMetrics <- c("findbugsAll","pmdPriority","pmdCodeSize","pmdDesign","pmdCou
 
 ## Perform the classifier computation, evaluation and selection ##
 
-# n corresponds to the level for which you want to compute model, e.g. n = 5 corresponds to models with 5 metrics
+# n corresponds to the level for which you want to compute model, e.g. n = 5 corresponds to models with 5 metrics. You need to adjust this value if you want to compute models for a different level.
 n <- 5
 combinations <- combn(miningMetricsIssues, n)
-# response is the response variable you want to build a classifier for. For the data, you can selected between "Cause", "Total", "source", "Target"
-response <- "Cause"
+# This is the response variable you want to build a classifier for. For the data, you can select between "Cause", "Total", "source", "Target".
+response <- "Total"
 
 # The result variables in which the optimal model will be stored after the execution of the loop. You can query these variables to read the results of the run
+
+# The F-score of the optimal model
 maxF <- 0
+# The recall of the optimal model
 maxRecall <- 0
+# The precision of the optimal model
 maxPrecision <- 0
+# The metrics that make up the optimal model
 metrics <- list()
+# The optimal model with descriptive data
 model <- list()
+# A summary of the optimal classifier and the confusion matrix
 classifier <- list()
 
+# Compute every combination of metrics and see if it is better than the prior maximum
 for(i in 1:(length(combinations)/n)) {
-  # print a status indicator
+  # Print a progress indicator
   print(i)
   
-  # select a combination of metrics from the data
+  # Select a combination of metrics from the data
   tempData <- data[combinations[,i]]
   
-  # build and evaluate the classifier
+  # Build and evaluate the classifier
   tempModel <- NB(as.logical(data[, response]) ~ ., data=tempData)
   tempClassifier <- evaluate_Weka_classifier(tempModel, numFolds = 10, seed = 1)
   
-  # compute precision and recall from the confusion matrix
+  # Compute precision and recall from the confusion matrix
   truePositive = tempClassifier$confusionMatrix[2, 2]
   falsePositive = tempClassifier$confusionMatrix[1, 2]
   falseNegative = tempClassifier$confusionMatrix[2, 1]
   
   precision = truePositive / (truePositive + falsePositive)
   if((truePositive + falsePositive) == 0) {
+    # This is needed, because in case of 0, R will assign the value "na" and further comparison operations will fail
     precision = 0
   }
   recall = truePositive / (truePositive + falseNegative)
@@ -58,13 +71,15 @@ for(i in 1:(length(combinations)/n)) {
     fMeasure = 0
   }
   
-  # see if the current model is more optimal than the current max
+  # See if the current model is better than the current maximum
   if(fMeasure > maxF) {
     maxF <- fMeasure
     maxRecall <- recall
     maxPrecision <-  precision
-    metrics <- el
+    metrics <- combinations[,i]
     model <- tempModel
     classifier <- tempClassifier
   }
 }
+
+# When this loop has executed, you can query the result variables (maxF, maxRecall, maxPrecision, metrics, model, classifier) to get information about the optimal model of the current run
